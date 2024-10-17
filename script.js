cbd315a660c6dd40c89bb4ef796522ded877bb08
razumcevvaa <katerazumceva@gmail.com>
2024-10-17 21:49:51 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector('.nav-item')
+const toggle = document.getElementById('menu-toggle')
+console.log(menu)
+menu.addEventListener('click', ()=>{
+  toggle.click()
+})
